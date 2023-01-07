5c950afe98aee77d28c00413f2aeb104368f1c75
MMaidul <maidulmuqtadir360@gmail.com>
2023-01-07 12:37:05 +0600

    database create

---
+++ database/migrations/2023_01_07_054820_create_exams_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\ForeignKeyDefinition;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('course_id');
+            $table->text('description');
+            $table->timestamps();
+
+            $table
+                ->foreign('couse_id')
+                ->references('id')
+                ->on('courses')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exams');
+    }
+};
