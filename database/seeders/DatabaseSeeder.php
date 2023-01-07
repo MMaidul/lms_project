c1e68efa5aebd22d6e8083786e799c483a0d3cc8
MMaidul <maidulmuqtadir360@gmail.com>
2023-01-07 16:56:16 +0600

    roles create

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +18,24 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $user = new User();
+        $user->name = 'Super Admin';
+        $user->email = 'super@admin.com';
+        $user->password = bcrypt('password');
+        $user->save();
+
+        $role = Role::create([
+            'name' => 'Super Admin',
+        ]);
+
+        $permission = Permission::create([
+            'name' => 'create-admin',
+        ]);
+
+        $role->givePermissionTo($permission);
+        $permission->assignRole($role);
+
+        $user->assignRole($role);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
